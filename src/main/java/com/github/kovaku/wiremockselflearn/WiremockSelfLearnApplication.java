19089bb590a9f15863318149155446558d0108e1
kovaku <kovaku@gmail.com>
2019-02-17 21:25:47 +0100

    Initial commit

---
+++ src/main/java/com/github/kovaku/wiremockselflearn/WiremockSelfLearnApplication.java
@@ -0,0 +1,13 @@
+package com.github.kovaku.wiremockselflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiremockSelfLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiremockSelfLearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/kovaku/wiremockselflearn/WiremockSelfLearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.kovaku.wiremockselflearn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WiremockSelfLearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
